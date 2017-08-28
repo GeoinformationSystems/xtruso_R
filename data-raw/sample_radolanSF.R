4834ddb41619b14c0c60d12ab88f600ef6880314
Stefan <stefan.wiemann@tu-dresden.de>
2017-08-28 16:26:59 +0200

    update

--- data-raw/sample_radolanSF.R
+++
@@ -1,7 +0,0 @@
-library(raster)
-library(xtruso)
-
-#read radolan data
-sample_radolanSF <- radolan_readBinarySF("data-raw/raa01-sf_10000-1708210650-dwd---bin")$raster
-#store data
-save(sample_radolanSF, file = "data/sample_radolanSF.Rdata")
